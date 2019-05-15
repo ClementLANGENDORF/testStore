c2769b115c4797af7c543979d49fd2dadd2d2027
clangend <clement.langendorf@gmail.com>
2019-05-15 16:22:48 +0200

    commit Dev

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
-import { AppRegistry, FlatList, StyleSheet, Text, View, ScrollView, Image, TouchableHighlight } from "react-native";
-import { StackNavigator } from "react-navigation";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import Constants from '../../constants/Constants';
+import React, {Component} from "react";
+import {FlatList, Image, ScrollView, StyleSheet, Text, TouchableHighlight, View} from "react-native";
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import LoadingAnimation from '../../img/cart-loading.gif';
 import * as ProductAction from '../../actions/ProductAction';
 
@@ -15,84 +12,86 @@ class ProductsList extends Component {
     };
 
     componentDidMount() {
-     this.props.ProductAction.getProducts();
-  }
+        this.props.ProductAction.getProducts();
+    }
 
-  _keyExtractor = (item, index) => item.id;
+    _keyExtractor = (item, index) => item.id;
 
-  render() {
-    const { navigate } = this.props.navigation;
-    const { products } = this.props;
-    const Items = <FlatList contentContainerStyle={styles.list} numColumns={1}
-      data={products.filter(item => item.categories[0].name === this.props.navigation.state.params.product.name)}
-      keyExtractor={this._keyExtractor}
-      renderItem={({ item }) =>
-      <TouchableHighlight style={{width:'75%'}} onPress={() => navigate("Product", { product: item })} underlayColor="white">
-        <View style={styles.view} >
-          {console.log(item)}
-          <Image style={styles.image} source={{uri: item.images[0].src}} />
-          <Text style={styles.text}>{item.name}</Text>
-          <Text style={styles.textPrice}>{item.regular_price}</Text>
-          <Text style={styles.textPrice}>{item.price}</Text>
-        </View>
-      </TouchableHighlight>
-      }
-    />;
-    return (
-      <ScrollView style={{flex:1}}>
-        {this.props.products.length ? Items :
-          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Image style={styles.loader} source={LoadingAnimation}/>
-          </View>
-        }
-      </ScrollView>
-    );
-  }
+    render() {
+        const {navigate} = this.props.navigation;
+        const {products} = this.props;
+        const Items = <FlatList contentContainerStyle={styles.list} numColumns={1}
+                                data={products.filter(item => item.categories[0].name === this.props.navigation.state.params.product.name)}
+                                keyExtractor={this._keyExtractor}
+                                renderItem={({item}) =>
+                                    <TouchableHighlight style={{width: '75%'}}
+                                                        onPress={() => navigate("Product", {product: item})}
+                                                        underlayColor="white">
+                                        <View style={styles.view}>
+                                            {console.log(item)}
+                                            <Image style={styles.image} source={{uri: item.images[0].src}}/>
+                                            <Text style={styles.text}>{item.name.toUpperCase()}</Text>
+                                            <Text style={styles.textPrice}>{item.regular_price}</Text>
+                                            <Text style={styles.textPrice}>{item.price}</Text>
+                                        </View>
+                                    </TouchableHighlight>
+                                }
+        />;
+        return (
+            <ScrollView style={{flex: 1}}>
+                {this.props.products.length ? Items :
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Image style={styles.loader} source={LoadingAnimation}/>
+                    </View>
+                }
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  list: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10
-  },
-  loader: {
-    width: 200,
-    height: 200,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 150,
-    height: 150
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 15,
-    padding: 5
-  },
-  textPrice: {
-    textAlign: 'center',
-    fontSize: 10,
-    padding: 5
-  },
+    list: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    view: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10
+    },
+    loader: {
+        width: 200,
+        height: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 150,
+        height: 150
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 15,
+        padding: 5
+    },
+    textPrice: {
+        textAlign: 'center',
+        fontSize: 10,
+        padding: 5
+    },
 });
 
 function mapStateToProps(state) {
-	return {
-		products: state.products
-	};
+    return {
+        products: state.products
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		ProductAction: bindActionCreators(ProductAction, dispatch)
-	};
+    return {
+        ProductAction: bindActionCreators(ProductAction, dispatch)
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductsList);
